ea20949637aca6acd7792601aa310894d3223485
User <user@Users-MacBook-Pro.local>
2019-03-19 11:45:09 +0530

    Home page

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+  constructor(private geolocation: Geolocation) {}
+  
+  locate(){
+    this.geolocation.getCurrentPosition().then((resp) => {
+      console.log("lat" + resp.coords.latitude + "- long" + resp.coords.longitude)
+      return resp.coords.latitude + "and" + resp.coords.longitude;
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+  }
+}
